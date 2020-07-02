eb2d4c571a32c8c5576781d00eb35e9d530049ff
Eugene Ignatov <ignatovyevgen@gmail.com>
2020-07-02 17:04:19 -0500

    can not

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 function sum(num1 = 0, num2 = 0) {
-	num1 = parseInt(num1) || 0;
-	num2 = parseInt(num2) || 0;
-	if (Array.isArray(num1) || typeof num1 === 'object') {
-		num1 = 0;
-	} else if (Array.isArray(num2) || typeof num2 === 'object') {
-		num2 = 0;
-	}
+  num1 = parseInt(num1) || 0;
+  num2 = parseInt(num2) || 0;
+  if (Array.isArray(num1) || typeof num1 === 'object') {
+    num1 = 0;
+  } else if (Array.isArray(num2) || typeof num2 === 'object') {
+    num2 = 0;
+  }
 
-	return num1 + num2;
+  return num1 + num2;
 }
-//some test for
+//some test for lst
 module.exports = sum;
